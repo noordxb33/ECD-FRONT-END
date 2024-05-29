2cd4822e884b0f52339cd904c7952a3dc0787d17
noordxb33 <noorullah.azizidxb@gmail.com>
2024-05-29 15:58:04 +0430

    final Commit
    I have added yearPage

--- src/Pages/Acessories/YearPage/Year.js
+++ src/Pages/Acessories/YearPage/Year.js
@@ -6,7 +6,22 @@ import Button from '@mui/material/Button';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import CancelIcon from '@mui/icons-material/Cancel';
 
-export default function Year(props) {
+
+export default function YearPage(props){
+
+
+    return (
+        <Grid container>
+            <Year getRequest={props.getRequest} postRequest={props.postRequest}/>
+
+            <YearTable/>
+        </Grid>
+    
+    )
+}
+
+
+function Year(props) {
 
 
     return (
@@ -59,3 +74,83 @@ export default function Year(props) {
         </FormContainer>
     )
 }
+
+
+
+function YearTable() {
+    return (
+      <MaterialTable
+        title="Multiple Detail Panels Preview"
+        columns={[
+          { title: 'Name', field: 'name' },
+          { title: 'Surname', field: 'surname' },
+          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+          {
+            title: 'Birth Place',
+            field: 'birthCity',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          },
+        ]}
+        data={[
+          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+        ]}
+        detailPanel={[
+          {
+            tooltip: 'Show Name',
+            render: rowData => {
+              return (
+                <div
+                  style={{
+                    fontSize: 100,
+                    textAlign: 'center',
+                    color: 'white',
+                    backgroundColor: '#43A047',
+                  }}
+                >
+                  {rowData.name}
+                </div>
+              )
+            },
+          },
+          {
+            icon: 'account_circle',
+            tooltip: 'Show Surname',
+            render: rowData => {
+              return (
+                <div
+                  style={{
+                    fontSize: 100,
+                    textAlign: 'center',
+                    color: 'white',
+                    backgroundColor: '#E53935',
+                  }}
+                >
+                  {rowData.surname}
+                </div>
+              )
+            },
+          },
+          {
+            icon: 'favorite_border',
+            openIcon: 'favorite',
+            tooltip: 'Show Both',
+            render: rowData => {
+              return (
+                <div
+                  style={{
+                    fontSize: 100,
+                    textAlign: 'center',
+                    color: 'white',
+                    backgroundColor: '#FDD835',
+                  }}
+                >
+                  {rowData.name} {rowData.surname}
+                </div>
+              )
+            },
+          },
+        ]}
+      />
+    )
+  }
